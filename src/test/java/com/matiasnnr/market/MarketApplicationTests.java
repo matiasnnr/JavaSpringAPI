0c825a159f5f6ba865847ba52e260e9ae0e2169a
matiasnnr <matias.nnr@hotmail.com>
2020-09-19 21:03:29 -0300

    hola mundo

---
+++ src/test/java/com/matiasnnr/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasnnr.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
